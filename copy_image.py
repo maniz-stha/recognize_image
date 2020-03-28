6b3beee52a19b3b8ba057270b7c4321505bdd8bd
Manish Shrestha <manishshrestha@whitehatengineering.com>
2020-03-28 21:35:18 +0545

    Copy your image using face recognition library

---
+++ copy_image.py
@@ -0,0 +1,20 @@
+import face_recognition
+import glob
+from shutil import copy
+
+image = face_recognition.load_image_file("known_images/manish.jpg")
+manish_face_encoding = face_recognition.face_encodings(image)[0]
+
+unknown_images = glob.glob('sample_images/*')
+
+for image in unknown_images:
+    faces = face_recognition.load_image_file(image)
+
+    face_locations = face_recognition.face_locations(faces)
+    face_encodings = face_recognition.face_encodings(faces, face_locations)
+    for face_encoding in face_encodings:
+        result = face_recognition.compare_faces([manish_face_encoding], face_encoding, 0.5)
+        if True in result:
+            file_name = image.split('/')[-1]
+            print('matched image' + file_name)
+            copy(image, 'matched_images/' + file_name)
