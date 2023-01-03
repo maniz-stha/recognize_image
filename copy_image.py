37eeed447032877e2a9d13646f63b4744a40af77
Manish Shrestha <maniz.stha@gmail.com>
2023-01-03 22:23:23 +0545

    Rename variable

--- copy_image.py
+++ copy_image.py
@@ -3,7 +3,7 @@ import glob
 from shutil import copy
 
 image = face_recognition.load_image_file("known_images/image.jpg")
-manish_face_encoding = face_recognition.face_encodings(image)[0]
+known_face_encoding = face_recognition.face_encodings(image)[0]
 
 unknown_images = glob.glob('unknown_images/*')
 
@@ -13,7 +13,7 @@ for image in unknown_images:
     face_locations = face_recognition.face_locations(faces)
     face_encodings = face_recognition.face_encodings(faces, face_locations)
     for face_encoding in face_encodings:
-        result = face_recognition.compare_faces([manish_face_encoding], face_encoding, 0.5)
+        result = face_recognition.compare_faces([known_face_encoding], face_encoding, 0.5)
         if True in result:
             file_name = image.split('/')[-1]
             print('matched image' + file_name)
